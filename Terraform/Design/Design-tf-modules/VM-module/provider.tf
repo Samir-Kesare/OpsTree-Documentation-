a6078fab2278bcd66d23d492740770933ae4d9e5
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-03-05 07:42:19 +0530

    Update provider.tf

--- Terraform/Design/Design-tf-modules/VM-module/provider.tf
+++ Terraform/Design/Design-tf-modules/VM-module/provider.tf
@@ -1,1 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.38.0"  # Using a minimum version constraint
+    }
+  }
+}
 
+provider "aws" {
+  region = "ap-northeast-1"  
+} 
